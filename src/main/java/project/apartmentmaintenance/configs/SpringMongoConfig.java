009e49c0041f62e9a47f8800f337312b9788b227
apmiller111@gmail.com <Bellum223&G4>
2023-12-06 15:42:53 -0500

    creation

---
+++ src/main/java/project/apartmentmaintenance/configs/SpringMongoConfig.java
@@ -0,0 +1,20 @@
+package project.apartmentmaintenance.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+// Credit to Srinithi K on https://stackoverflow.com/questions/23517977/spring-boot-mongodb-how-to-remove-the-class-column
+
+@Configuration
+public class SpringMongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory databaseFactory, MappingMongoConverter converter) {
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(databaseFactory, converter);
+    }
+}
---
+++ src/test/java/project/apartmentmaintenance/ApartmentMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.apartmentmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentMaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
